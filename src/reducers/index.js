afb2b6efe410bf893696b00dc7e70635b7b2a85a
Alexander Rosa <alexanderrosa@alexanders-mbp.lan>
2017-09-13 19:30:23 -0600

    feat: adding calendar UI

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,23 @@
+import { combineReducers } from 'redux'
+
 import {
   ADD_RECIPE,
   REMOVE_FROM_CALENDAR,
 } from '../actions';
 
+function food (state = {}, action) {
+  switch (action.type) {
+    case ADD_RECIPE:
+      const { recipe } = action
+
+      return {
+        ...state,
+        [recipe.label]: recipe
+      }
+    default:
+      return state
+  }
+}
 
 const initialCalendarState = {
   sunday: {
@@ -43,7 +58,7 @@ const initialCalendarState = {
 }
 
 function calendar(state=initialCalendarState, action) {
-  const { day, recipe, meal } = action
+  const { day, recipe, meal } = action;
 
   switch (action.type) {
     case ADD_RECIPE:
@@ -63,8 +78,11 @@ function calendar(state=initialCalendarState, action) {
         }
       }
     default:
-      return state;
+      return state
   }
 }
 
-export default calendar;
+export default combineReducers({
+  food,
+  calendar,
+})
